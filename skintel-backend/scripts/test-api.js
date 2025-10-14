42087ce118fe7441ed8df4d1f772210d29014506
comethrusws <basabjha.dev@gmail.com>
2025-10-14 17:13:20 +0545

    fixed: testapi script

--- skintel-backend/scripts/test-api.js
+++ skintel-backend/scripts/test-api.js
@@ -244,7 +244,7 @@ async function testGetOnboardingState() {
 async function testUserSignup() {
   const response = await makeRequest('POST', '/v1/auth/signup', {
     session_id: sessionId,
-    email: `basab2@mail.com`,
+    email: `${uuidv4()}@mail.com`,
     password: 'basabhaha123'
   });
 
