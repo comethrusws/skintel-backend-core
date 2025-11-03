759ad4dd5aa834e6446669f8ed425096925a2e43
comethrusws <basabjha.dev@gmail.com>
2025-11-03 15:11:19 +0545

    updaed: max request size

--- skintel-backend/src/index.ts
+++ skintel-backend/src/index.ts
@@ -21,8 +21,8 @@ const port = process.env.PORT || 3000;
 
 app.use(helmet());
 app.use(cors());
-app.use(express.json({ limit: '10mb' }));
-app.use(express.urlencoded({ extended: true, limit: '10mb' }));
+app.use(express.json({ limit: process.env.MAX_REQUEST_SIZE }));
+app.use(express.urlencoded({ extended: true, limit: process.env.MAX_REQUEST_SIZE }));
 
 app.use('/v1/sessions', sessionsRouter);
 app.use('/v1/onboarding', onboardingRouter);
